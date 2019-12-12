700b2faca0925db6b191c9b4ff90ae6100d7157a
sa100 <sergio.rudenko@gmail.com>
2019-12-12 18:58:41 +0300

    +dashboard draft

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,99 +1,173 @@
 
 <template>
-    <v-content>
-        <!-- <v-row justify="center">
-            <div style="height: 100%">
-                <v-dialog v-model="dialog" persistent max-width="290">
-                    <template v-slot:activator="{ on }">
-                        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-                    </template>
-                    <v-card>
-                        <v-card-title class="headline">Use Google's location service?</v-card-title>
-                        <v-card-text>
-                            Let Google help apps determine location. This means
-                            sending anonymous location data to Google, even when
-                            no apps are running.
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-                            <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
+    <span>
+        <div class="dashboard">
+            <div class="infobox absolute-container">header</div>
+            <div class="buttons absolute-container">
+                <div class="dashboard-button button-top-left">tl</div>
+                <div class="dashboard-button button-top-right">tr</div>
+                <div class="dashboard-button button-bottom-left">bl</div>
+                <div class="dashboard-button button-bottom-right">br</div>
             </div>
-        </v-row>-->
-        <v-row justify="center">
-            <v-card height="80vmin" width="90%">text</v-card>
-        </v-row>
-    </v-content>
-    <!-- <v-content>
-    <v-container class="fill-height" fluid>-->
-    <!-- <v-row align="center" justify="center">
-                <v-col cols="12" sm="8" md="4">
-                    <v-card class="elevation-12" width="90%">
-                        <v-toolbar color="primary" dark flat>
-                            <v-toolbar-title>Login form</v-toolbar-title>
-                            <v-spacer />
-                            <v-tooltip bottom>
-                                <template v-slot:activator="{ on }">
-                                    <v-btn
-                                        :href="source"
-                                        icon
-                                        large
-                                        target="_blank"
-                                        v-on="on"
-                                    >
-                                        <v-icon>mdi-code-tags</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>Source</span>
-                            </v-tooltip>
-                            <v-tooltip right>
-                                <template v-slot:activator="{ on }">
-                                    <v-btn
-                                        icon
-                                        large
-                                        href="https://codepen.io/johnjleider/pen/pMvGQO"
-                                        target="_blank"
-                                        v-on="on"
-                                    >
-                                        <v-icon>mdi-codepen</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>Codepen</span>
-                            </v-tooltip>
-                        </v-toolbar>
-                        <v-card-text>
-                            <v-form>
-                                <v-text-field
-                                    label="Login"
-                                    name="login"
-                                    prepend-icon="mdi-account"
-                                    type="text"
-                                />
-                                <v-text-field
-                                    id="password"
-                                    label="Password"
-                                    name="password"
-                                    prepend-icon="mdi-cellphone-iphone"
-                                    type="password"
-                                />
-                            </v-form>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer />
-                            <v-btn color="primary">Login</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-    </v-row>-->
-    <!-- </v-container>
-    </v-content>-->
+            <div class="indicator absolute-container">
+                <indicator></indicator>
+            </div>
+            <div class="footer absolute-container">footer</div>
+        </div>
+    </span>
 </template>
 
+<style scoped>
+  .relative-container {
+      position: relative;
+  }
+
+  .absolute-container {
+      position: absolute;
+  }
+
+  .dashboard {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      /* background: yellow; */
+  }
+
+  .dashboard-button {
+      position: absolute;
+      background: transparent;
+
+      width: 20vmin;
+      height: 20vmin;
+  }
+
+  /* Portrait */
+  @media screen and (orientation: portrait) {
+      .infobox {
+          top: 0;
+          left: 1%;
+          width: 98%;
+          height: 20%;
+          background: blueviolet;
+      }
+
+      .indicator {
+          top: calc(50% - 35vmin);
+          left: calc(50% - 35vmin);
+          width: 70vmin;
+          height: 70vmin;
+          /* background: green; */
+      }
+
+      .footer {
+          top: 80%;
+          left: 1%;
+          width: 98%;
+          height: 20%;
+          background: blue;
+      }
+
+      .buttons {
+          top: 20%;
+          left: 0;
+          width: 100%;
+          height: 60%;
+          /* background: red; */
+      }
+
+      .button-top-left {
+          top: calc(15% - 10vmin);
+          left: calc(15% - 10vmin);
+          background: chartreuse;
+      }
+
+      .button-top-right {
+          top: calc(15% - 10vmin);
+          left: calc(85% - 10vmin);
+          background: chartreuse;
+      }
+
+      .button-bottom-left {
+          top: calc(85% - 10vmin);
+          left: calc(15% - 10vmin);
+          background: chartreuse;
+      }
+
+      .button-bottom-right {
+          top: calc(85% - 10vmin);
+          left: calc(85% - 10vmin);
+          background: chartreuse;
+      }
+  }
+
+  /* Landscape */
+  @media screen and (orientation: landscape) {
+      .infobox {
+          top: 0;
+          left: 50%;
+          width: 50%;
+          height: 20%;
+          background: blueviolet;
+      }
+
+      .indicator {
+          top: calc(50% - 35vmin);
+          left: calc(25% - 35vmin);
+          width: 70vmin;
+          height: 70vmin;
+          /* background: green; */
+      }
+
+      .footer {
+          top: 80%;
+          left: 50%;
+          width: 50%;
+          height: 20%;
+          background: blue;
+      }
+
+      .buttons {
+          top: 20%;
+          left: 50%;
+          width: 50%;
+          height: 60%;
+          /* background: red; */
+      }
+
+      .button-top-left {
+          top: calc(25% - 10vmin);
+          left: calc(50% - 30vmin);
+          background: chartreuse;
+      }
+
+      .button-top-right {
+          top: calc(25% - 10vmin);
+          left: calc(50% + 10vmin);
+          background: chartreuse;
+      }
+
+      .button-bottom-left {
+          top: calc(75% - 10vmin);
+          left: calc(50% - 30vmin);
+          background: chartreuse;
+      }
+
+      .button-bottom-right {
+          top: calc(75% - 10vmin);
+          left: calc(50% + 10vmin);
+          background: chartreuse;
+      }
+  }
+</style>
+
 <script>
+import Indicator from '@/components/DashboardIndicator.vue';
+
 export default {
+    components: {
+        Indicator
+    },
+
     data() {
         return {
             dialog: false
